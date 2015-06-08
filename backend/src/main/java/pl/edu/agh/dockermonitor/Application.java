0f8aaa56d8f1310a173de6f107bba7b8bc366eb3
novy154 <slnowak154@gmail.com>
2015-06-08 16:42:33 +0200

    Backend setup

---
+++ backend/src/main/java/pl/edu/agh/dockermonitor/Application.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.dockermonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by novy on 08.06.15.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
