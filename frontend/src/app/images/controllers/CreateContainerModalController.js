d366778f934851bc15e2ed546d83cafe529a60bf
novy154 <slnowak154@gmail.com>
2015-06-10 23:30:33 +0200

    handled optional fields properly

--- frontend/src/app/images/controllers/CreateContainerModalController.js
+++ frontend/src/app/images/controllers/CreateContainerModalController.js
@@ -4,22 +4,47 @@
 
 'use strict';
 
+let objectAssign = require('object-assign');
+
 let CreateContainerModalController = ($scope, $modalInstance, imageId) => {
 
   $scope.containerData = {
-    name: null,
-    imageId: imageId,
-    command: null,
-    ports: null
+    imageId: imageId
+  };
+
+  let nonEmptyValue = (value) => {
+    return value !== undefined && value !== null && value !== '';
+  };
+
+  let withoutEmptyFields = (containerData) => {
+    let nullableFields = ['name', 'command'];
+
+    let containerDataCopy = objectAssign({}, containerData);
+    nullableFields.forEach(field => {
+      if (!nonEmptyValue(containerDataCopy[field])) {
+        delete containerDataCopy[field];
+      }
+    });
+
+    let portsProperty = 'ports';
+    if (containerDataCopy.hasOwnProperty(portsProperty) &&
+      (!nonEmptyValue(containerDataCopy[portsProperty].outer) || !nonEmptyValue(containerDataCopy[portsProperty].inner))) {
+
+      delete containerDataCopy[portsProperty]
+    }
+
+    return containerDataCopy;
   };
 
   $scope.submit = () => {
-    $modalInstance.close($scope.containerData);
+    $modalInstance.close(withoutEmptyFields($scope.containerData));
   };
 
   $scope.cancel = () => {
     $modalInstance.dismiss('cancel');
   };
+
+  $scope.nonEmptyValue = nonEmptyValue;
 };
 
 CreateContainerModalController.$inject = ['$scope', '$modalInstance', 'imageId'];
